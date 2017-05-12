efb4cb34c8a328408eb1539653a127124844d8fc
malinink <apmath@malinink.ru>
2017-05-12 23:07:58 +0300

    init exam project

---
+++ src/main/java/exam2017/Exam2017Application.java
@@ -0,0 +1,16 @@
+package exam2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author malinink
+ */
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@SpringBootApplication
+public class Exam2017Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Exam2017Application.class, args);
+    }
+}
+
---
+++ src/test/java/exam2017/Exam2017ApplicationTests.java
@@ -0,0 +1,17 @@
+package exam2017;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author malinink
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Exam2017ApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
