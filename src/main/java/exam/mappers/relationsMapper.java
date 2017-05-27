df21525aabcf4a87a264702f76db8a8095d39346
Valentin <schulga.valentin@yandex.ru>
2017-05-27 14:42:25 +0300

    added a request in the binder maper

--- src/main/java/exam/mappers/relationsMapper.java
+++ src/main/java/exam/mappers/relationsMapper.java
@@ -1,8 +1,17 @@
 package exam.mappers;
 
+import exam.entities.StudentEntity;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
 /**
  *
  */
 public interface relationsMapper {
+
+    @Select("SELECT hobby_id FROM hobbys_students WHERE student_id=#{studentId}")
+    List<Integer> find(Integer studentId);
+    
 
 }
