8eb97b8b79940e6e422add533968718a2cc655f3
Your0Liya@yandex.ru <liyagulechka97>
2017-06-02 19:28:44 +0300

    asd

--- src/main/resources/migrations/scripts/20170602104655_create_brands_table.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS brands CASCADE;
-
-CREATE TABLE brands(
-  id  SERIAL  PRIMARY KEY,
-  name VARCHAR(40) NOT NULL UNIQUE,
-  foundationDate SMALLINT NOT NULL CHECK(foundationDate>0 AND foundationDate<2017),
-  premium VARCHAR(5) NOT NULL CHECK (premium='yes' OR premium='no')
-);
