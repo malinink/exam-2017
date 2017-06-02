c33381641e5fb2777a4f516bbfa9a0539de5e3e1
VanilaBear <eralex09@gmail.com>
2017-06-02 16:54:02 +0300

    Created new entity FlatExt

---
+++ src/main/java/exam/entities/FlatExt.java
@@ -0,0 +1,16 @@
+package exam.entities;
+
+/**
+ * Created by alex on 02.06.17.
+ */
+public class FlatExt extends Flat {
+
+    private int livingsquare;
+
+    public int getLivingsquare() {
+        return livingsquare;
+    }
+    public void setLivingsquare(int livingsquare) {
+        this.livingsquare = livingsquare;
+    }
+}
---
+++ src/main/java/exam/mappers/FlatExtMapper.java
@@ -0,0 +1,7 @@
+package exam.mappers;
+
+/**
+ * Created by alex on 02.06.17.
+ */
+public class FlatExtMapper {
+}
