9eccc819adfadf9de61dd586fb13c8098e05080c
VanilaBear <eralex09@gmail.com>
2017-06-02 14:28:14 +0300

    Update: added new request mapping

--- src/main/java/exam/Controllers.java
+++ src/main/java/exam/Controllers.java
@@ -1,10 +1,13 @@
 package exam;
 
 import exam.entities.Flat;
+import exam.entities.Room;
 import exam.mappers.FlatMapper;
+import exam.mappers.RoomMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,11 +22,21 @@ public class Controllers {
     @Autowired
     private FlatMapper flatMapper;
 
+    @Autowired
+    private RoomMapper roomMapper;
+
     @RequestMapping("/flats")
     public String showAllFlats(Model model) {
         List<Flat> flats = flatMapper.findAll();
         model.addAttribute("flats", flats);
         return "flats/list";
+    }
+
+    @RequestMapping("/flats/{id}")
+    public String showRoom(Model model, @PathVariable int id) {
+        List<Room> rooms = roomMapper.showRooms(id);
+        model.addAttribute("rooms", rooms);
+        return "rooms/list2";
     }
 
 }
